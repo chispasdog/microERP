4b259195e6244e400737bcf0a3f574eeb2b6d840
chispasdog <127110825+chispasdog@users.noreply.github.com>
2023-12-26 14:00:44 +0100

    ultimo cambio del repitorio por el momento

--- demo/src/main/java/com/example/demo/repositorio/Repositoriopedido.java
+++ demo/src/main/java/com/example/demo/repositorio/Repositoriopedido.java
@@ -1,9 +1,15 @@
 package com.example.demo.repositorio;
 
 import com.example.demo.modelo.Pedido;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface Repositoriopedido extends JpaRepository<Pedido,Long> {
+
+
+
 }
